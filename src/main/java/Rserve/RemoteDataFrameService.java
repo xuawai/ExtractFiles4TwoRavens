04b93ab3d97eb447d4518f4175397d9beebc2caa
FoxerLee <foxerlee1@gmail.com>
2017-11-06 13:53:14 +0800

    add prep handle

---
+++ src/main/java/Rserve/RemoteDataFrameService.java
@@ -0,0 +1,209 @@
+package Rserve;
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+
+import org.rosuda.REngine.*;
+import org.rosuda.REngine.Rserve.*;
+
+public class RemoteDataFrameService {
+    private static Logger dbgLog = Logger.getLogger(RemoteDataFrameService.class.getPackage().getName());
+//    private static String TMP_TABDATA_FILE_EXT = ".tab";
+//    private static String TMP_RDATA_FILE_EXT = ".RData";
+    private static String PREPROCESS_FILE_PREFIX = "dataversePreprocess_";
+    public static String LOCAL_TEMP_DIR = System.getProperty("java.io.tmpdir");
+
+    // 配置Rserve文件
+    private static String RSERVE_HOST = "127.0.0.1";
+    private static String RSERVE_USER = null;
+    private static String RSERVE_PWD = null;
+    private static int    RSERVE_PORT = 6311;
+
+    private static String DATAVERSE_R_FUNCTIONS = "scripts/dataverse_r_functions.R";
+    private static String DATAVERSE_R_PREPROCESSING = "scripts/preprocess.R";
+
+    public static String RSERVE_TMP_DIR="/Users/liyuan/Documents/Junk/Rserv";
+
+    public String PID = null;
+    public String tempFileNameIn = "test";
+    public String tempFileNameOut = "result";
+
+
+    //如果工作文件夹路径不存在就创建
+    public void setupWorkingDirectory(RConnection c) {
+
+        try {
+            // check the temp directory; try to create it if it doesn't exist:
+
+            String checkWrkDir = "if (!file_test('-d', '" + RSERVE_TMP_DIR + "')) {dir.create('" + RSERVE_TMP_DIR + "', showWarnings = FALSE, recursive = TRUE);}";
+            c.voidEval(checkWrkDir);
+
+        } catch (RserveException rse) {
+            rse.printStackTrace();
+        }
+    }
+
+    public int getFileSize(RConnection c, String targetFilename){
+        dbgLog.fine("targetFilename="+targetFilename);
+        int fileSize = 0;
+        try {
+            String fileSizeLine = "round(file.info('"+targetFilename+"')$size)";
+            fileSize = c.eval(fileSizeLine).asInteger();
+        } catch (RserveException rse) {
+            rse.printStackTrace();
+        } catch (REXPMismatchException mme) {
+            mme.printStackTrace();
+        }
+        return fileSize;
+    }
+
+    public File transferRemoteFile(RConnection c, String targetFilename,
+                                   String tmpFilePrefix, String tmpFileExt, int fileSize) {
+
+        // set up a local temp file:
+
+        File tmprsltfl = null;
+        String resultFile = tmpFilePrefix + PID + "." + tmpFileExt;
+
+        RFileInputStream ris = null;
+        OutputStream outbr = null;
+        try {
+            tmprsltfl = new File(LOCAL_TEMP_DIR, resultFile);
+            outbr = new BufferedOutputStream(new FileOutputStream(tmprsltfl));
+            // open the input stream
+            ris = c.openFile(targetFilename);
+
+            if (fileSize < 1024 * 1024 * 500) {
+                int bfsize = fileSize;
+                byte[] obuf = new byte[bfsize];
+                ris.read(obuf);
+                outbr.write(obuf, 0, bfsize);
+            }
+            ris.close();
+            outbr.close();
+            return tmprsltfl;
+        } catch (FileNotFoundException fe) {
+            fe.printStackTrace();
+            dbgLog.fine("FileNotFound exception occurred");
+            return tmprsltfl;
+        } catch (IOException ie) {
+            ie.printStackTrace();
+            dbgLog.fine("IO exception occurred");
+        } finally {
+            if (ris != null) {
+                try {
+                    ris.close();
+                } catch (IOException e) {
+
+                }
+            }
+
+            if (outbr != null) {
+                try {
+                    outbr.close();
+                } catch (IOException e) {
+
+                }
+            }
+
+        }
+
+        // delete remote file:
+
+        try {
+            String deleteLine = "file.remove('"+targetFilename+"')";
+            c.eval(deleteLine);
+        } catch (Exception ex) {
+            // do nothing.
+        }
+
+        return tmprsltfl;
+    }
+
+    public File runDataPreprocessing(String path) throws IOException {
+        File preprocessedDataFile = null;
+
+        File originFile = new File(path);
+
+        try {
+
+            // Set up an Rserve connection
+            // 在这里修改RSERVE_HOST，和 RSERVE_PORT, RSERVE_USER, RSERRVE_PWD
+            RConnection c = new RConnection(RSERVE_HOST, RSERVE_PORT);
+
+            InputStream in = new FileInputStream(originFile);
+
+            RFileOutputStream os = c.createFile(tempFileNameIn);
+
+            int bufsize;
+            byte[] bffr = new byte[4 * 8192];
+
+            while ((bufsize = in.read(bffr)) != -1) {
+                os.write(bffr, 0, bufsize);
+            }
+
+            in.close();
+            os.close();
+            String loadlib = "library(rjson)";
+            // 跳到这里
+            c.voidEval(loadlib);
+            String rscript = readLocalResource(DATAVERSE_R_PREPROCESSING);
+
+            c.voidEval(rscript);
+
+            String runPreprocessing = "json<-preprocess(filename=\""+ tempFileNameIn +"\")";
+
+            c.voidEval(runPreprocessing);
+
+            // Save the output in a temp file:
+
+            String saveResult = "write(json, file='"+ tempFileNameOut +"')";
+
+            c.voidEval(saveResult);
+
+            // Finally, transfer the saved file back on the application side:
+
+            int fileSize = getFileSize(c,tempFileNameOut);
+            preprocessedDataFile = transferRemoteFile(c, tempFileNameOut, PREPROCESS_FILE_PREFIX, "json", fileSize);
+
+            String deleteLine = "file.remove('"+tempFileNameOut+"')";
+            c.eval(deleteLine);
+
+            c.close();
+
+        } catch (RserveException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        return preprocessedDataFile;
+
+    }
+
+    private static String readLocalResource(String path) {
+
+        dbgLog.fine(String.format("Data Frame Service: readLocalResource: reading local path \"%s\"", path));
+
+        // Get stream
+        InputStream resourceStream = RemoteDataFrameService.class.getResourceAsStream(path);
+        String resourceAsString = "";
+
+        // Try opening a buffered reader stream
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(resourceStream, "UTF-8"));
+
+            String line = null;
+            while ((line = rd.readLine()) != null) {
+                resourceAsString = resourceAsString.concat(line + "\n");
+            }
+            resourceStream.close();
+        } catch (IOException ex) {
+            dbgLog.warning(String.format("RDATAFileReader: (readLocalResource) resource stream from path \"%s\" was invalid", path));
+        }
+
+        // Return string
+        return resourceAsString;
+    }
+}
